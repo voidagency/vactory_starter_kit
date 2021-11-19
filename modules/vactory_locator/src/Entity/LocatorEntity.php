b3667af3badf72759753cd9e6fb83c41976e5b9b
Brahim KHOUY <b.khouy@void.fr>
2021-11-19 16:53:54 +0100

    Add: Add revision metadata keys to locator entity annotation.

--- modules/vactory_locator/src/Entity/LocatorEntity.php
+++ modules/vactory_locator/src/Entity/LocatorEntity.php
@@ -53,6 +53,11 @@ use Drupal\user\UserInterface;
  *     "langcode" = "langcode",
  *     "status" = "status",
  *   },
+ *   revision_metadata_keys = {
+ *     "revision_user" = "revision_user",
+ *     "revision_created" = "revision_created",
+ *     "revision_log_message" = "revision_log",
+ *   },
  *   links = {
  *     "canonical" = "/locator_entity/{locator_entity}",
  *     "add-page" = "/admin/structure/locator_entity/add",
