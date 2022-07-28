82f5aba3245ffa021afa33b8fd715eb01a275112
h.bahlaouane <h.bahlaouane@void.fr>
2022-07-28 17:18:09 +0100

    feat(decoupled): only retrieve custom blocks.

--- modules/vactory_decoupled/src/BlocksManager.php
+++ modules/vactory_decoupled/src/BlocksManager.php
@@ -179,6 +179,10 @@ class BlocksManager
       }
     }
 
+    $blocks_list = array_filter($blocks_list, function($block) {
+      return strpos($block->getPluginId(), 'block_content:') !== false;
+    });
+
     $blocks = array_map(function ($block) use ($conditionPluginManager) {
       $visibility = $block->getVisibility();
 
