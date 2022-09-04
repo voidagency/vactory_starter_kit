507902f0f59548737940242bf8d7c36a3cbf22b2
h.bahlaouane <h.bahlaouane@void.fr>
2022-09-04 16:14:48 +0100

    feat(auth): cache jwt

--- modules/vactory_decoupled/src/SimpleOauthAuthenticationProvider.php
+++ modules/vactory_decoupled/src/SimpleOauthAuthenticationProvider.php
@@ -23,10 +23,10 @@ use Drupal\Core\Site\Settings;
  */
 class SimpleOauthAuthenticationProvider extends BaseSimpleOauthAuthenticationProvider
 {
-  /**
-   * @var Configuration
-   */
-  private $jwtConfiguration;
+    /**
+     * @var Configuration
+     */
+    private $jwtConfiguration;
 
     /**
      * The Social Auth user manager.
@@ -35,163 +35,193 @@ class SimpleOauthAuthenticationProvider extends BaseSimpleOauthAuthenticationPro
      */
     protected $userManager;
 
-  /**
-   * {@inheritdoc}
-   *
-   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
-   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
-   * @throws \Drupal\Core\Entity\EntityStorageException
-   * @throws \League\OAuth2\Server\Exception\OAuthServerException
-   */
-  public function authenticate(Request $request)
-  {
-    if ($request->headers->has("X-Auth-Provider")) {
-      $provider = $request->headers->get("X-Auth-Provider");
-      $authorization = $request->headers->get('authorization');
-      $jwt = trim(str_replace('Bearer ', '', $authorization));
-      $this->userManager = \Drupal::service("vactory_decoupled.user_manager");
+    /**
+     * The cache backend that should be used.
+     *
+     * @var \Drupal\Core\Cache\CacheBackendInterface
+     */
+    protected $cache;
 
-      if ($provider === "keycloak") {
-        return $this->authenticateKeycloak($jwt);
-      }
-      else if ($provider === "facebook") {
-        return $this->authenticateFacebook($jwt);
-      }
-      else if ($provider === "google") {
-        return $this->authenticateGoogle($jwt);
-      }
-    }
+    /**
+     * {@inheritdoc}
+     *
+     * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+     * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+     * @throws \Drupal\Core\Entity\EntityStorageException
+     * @throws \League\OAuth2\Server\Exception\OAuthServerException
+     */
+    public function authenticate(Request $request)
+    {
+        if ($request->headers->has("X-Auth-Provider")) {
+            $provider = $request->headers->get("X-Auth-Provider");
+            $authorization = $request->headers->get('authorization');
+            $jwt = trim(str_replace('Bearer ', '', $authorization));
+            $this->userManager = \Drupal::service("vactory_decoupled.user_manager");
+            $this->cache = \Drupal::cache('vactory_decoupled_oauth');
 
-    return parent::authenticate($request);
-  }
-
-  private function authenticateGoogle($jwt) {
-    // $result = \Drupal::httpClient()->get("https://graph.facebook.com/me?access_token=${jwt}&fields=id,name,email,picture");
-    $result = \Drupal::httpClient()->get("https://openidconnect.googleapis.com/v1/userinfo", [
-      'headers' => [
-        'Authorization' => "Bearer " .$jwt,
-      ],
-    ]);
-    $payload = json_decode((string) $result->getBody(), true);
-
-    $id = $payload["sub"];
-    $name = $payload["name"];
-    $first_name = $payload["given_name"];
-    $last_name = $payload["family_name"];
-    $email = $payload["email"];
-    $picture_url = $payload["picture"];
-    $username = (!empty($email)) ? $email : "${id}@google.com";
-    $mail = (!empty($email)) ? $email : "${id}@google.com";
-
-    $user = user_load_by_name($username);
-    if (!$user) {
-      $values = [
-        'status' => 1,
-        'name' => $username,
-        'mail' => $mail,
-        'field_first_name' => $first_name,
-        'field_last_name' => $last_name,
-      ];
-
-        if (!empty($picture_url) && $this->userManager->userPictureEnabled()) {
-            $file = $this->userManager->downloadProfilePic($picture_url, $id);
-            if ($file) {
-                $values['user_picture'] = $file->id();
+            if ($provider === "keycloak") {
+                return $this->authenticateKeycloak($jwt);
+            } else if ($provider === "facebook") {
+                return $this->authenticateFacebook($jwt);
+            } else if ($provider === "google") {
+                return $this->authenticateGoogle($jwt);
             }
         }
 
-      $user = User::create($values);
-      $user->save();
+        return parent::authenticate($request);
     }
-    return $user;
-  }
 
-  private function authenticateFacebook($jwt) {
-    // $result = \Drupal::httpClient()->get("https://graph.facebook.com/me?access_token=${jwt}&fields=id,name,email,picture");
-    $result = \Drupal::httpClient()->get("https://graph.facebook.com/me", [
-      'query' => [
-        'access_token' => $jwt,
-        'fields' => 'id,name,email,picture'
-      ],
-    ]);
-    $payload = json_decode((string) $result->getBody(), true);
-
-    $id = $payload["id"];
-    $name = $payload["name"];
-    list($first_name, $last_name) = array_pad( explode( ' ', $name ), 4, '' );
-    $email = $payload["email"];
-    $picture_url = $payload["picture"];
-    $username = (!empty($email)) ? $email : "${id}@facebook.com";
-    $mail = (!empty($email)) ? $email : "${id}@facebook.com";
-
-    $user = user_load_by_name($username);
-    if (!$user) {
-      $values = [
-        'status' => 1,
-        'name' => $username,
-        'mail' => $mail,
-        'field_first_name' => $first_name,
-        'field_last_name' => $last_name,
-      ];
-
-        if (isset($picture_url["data"]["url"]) && $this->userManager->userPictureEnabled()) {
-            $file = $this->userManager->downloadProfilePic($picture_url["data"]["url"], $id);
-            if ($file) {
-                $values['user_picture'] = $file->id();
-            }
+    private function authenticateGoogle($jwt)
+    {
+        $cacheKey = "google:" . $jwt;
+        if ($cache = $this->cache->get($cacheKey)) {
+            $payload = $cache->data;
+        } else {
+            $result = \Drupal::httpClient()->get("https://openidconnect.googleapis.com/v1/userinfo", [
+                'headers' => [
+                    'Authorization' => "Bearer " . $jwt,
+                ],
+            ]);
+            $payload = json_decode((string)$result->getBody(), true);
+            $ten_minutes = 60 * 10;
+            $this->cache->set($cacheKey, $payload, $this->getRequestTime() + $ten_minutes);
         }
 
-      $user = User::create($values);
-      $user->save();
+        $id = $payload["sub"];
+        $name = $payload["name"];
+        $first_name = $payload["given_name"];
+        $last_name = $payload["family_name"];
+        $email = $payload["email"];
+        $picture_url = $payload["picture"];
+        $username = (!empty($email)) ? $email : "${id}@google.com";
+        $mail = (!empty($email)) ? $email : "${id}@google.com";
+
+        $user = user_load_by_name($username);
+        if (!$user) {
+            $values = [
+                'status' => 1,
+                'name' => $username,
+                'mail' => $mail,
+                'field_first_name' => $first_name,
+                'field_last_name' => $last_name,
+            ];
+
+            if (!empty($picture_url) && $this->userManager->userPictureEnabled()) {
+                $file = $this->userManager->downloadProfilePic($picture_url, $id);
+                if ($file) {
+                    $values['user_picture'] = $file->id();
+                }
+            }
+
+            $user = User::create($values);
+            $user->save();
+        }
+        return $user;
     }
-    return $user;
-  }
 
-  private function authenticateKeycloak($jwt) {
-    $this->jwtConfiguration = Configuration::forSymmetricSigner(
-      new Sha256(),
-      InMemory::plainText('0eylDkJplsBm22Meby8EKIeBMckMKMyO')
-    );
+    private function authenticateFacebook($jwt)
+    {
+        $cacheKey = "facebook:" . $jwt;
+        if ($cache = $this->cache->get($cacheKey)) {
+            $payload = $cache->data;
+        } else {
+            $result = \Drupal::httpClient()->get("https://graph.facebook.com/me", [
+                'query' => [
+                    'access_token' => $jwt,
+                    'fields' => 'id,name,email,picture'
+                ],
+            ]);
+            $payload = json_decode((string)$result->getBody(), true);
+            $ten_minutes = 60 * 10;
+            $this->cache->set($cacheKey, $payload, $this->getRequestTime() + $ten_minutes);
+        }
 
-    // $keycloak_issuer = Settings::get('KEYCLOAK_ISSUER', "http://localhost:8003/auth/realms/master");
-    $keycloak_issuer = Settings::get('KEYCLOAK_ISSUER', "https://keycloak.lecontenaire.com/auth/realms/dev");
+        $id = $payload["id"];
+        $name = $payload["name"];
+        list($first_name, $last_name) = array_pad(explode(' ', $name), 4, '');
+        $email = $payload["email"];
+        $picture_url = $payload["picture"];
+        $username = (!empty($email)) ? $email : "${id}@facebook.com";
+        $mail = (!empty($email)) ? $email : "${id}@facebook.com";
 
-    $this->jwtConfiguration->setValidationConstraints(
-      new IssuedBy($keycloak_issuer));
+        $user = user_load_by_name($username);
+        if (!$user) {
+            $values = [
+                'status' => 1,
+                'name' => $username,
+                'mail' => $mail,
+                'field_first_name' => $first_name,
+                'field_last_name' => $last_name,
+            ];
 
-      try {
-        // Attempt to parse the JWT
-        $token = $this->jwtConfiguration->parser()->parse($jwt);
-      } catch (\Lcobucci\JWT\Exception $exception) {
-        throw OAuthServerException::accessDenied($exception->getMessage(), null, $exception);
-      }
+            if (isset($picture_url["data"]["url"]) && $this->userManager->userPictureEnabled()) {
+                $file = $this->userManager->downloadProfilePic($picture_url["data"]["url"], $id);
+                if ($file) {
+                    $values['user_picture'] = $file->id();
+                }
+            }
 
-      try {
-        // Attempt to validate the JWT
-        $constraints = $this->jwtConfiguration->validationConstraints();
-        $this->jwtConfiguration->validator()->assert($token, ...$constraints);
-      } catch (RequiredConstraintsViolated $exception) {
-        throw OAuthServerException::accessDenied('Access token could not be verified');
-      }
+            $user = User::create($values);
+            $user->save();
+        }
+        return $user;
+    }
 
-      $claims = $token->claims();
-      $username = $claims->get("preferred_username");
-      $mail = $claims->get("email", $username . "@keycloak.com");  
+    private function authenticateKeycloak($jwt)
+    {
+        $this->jwtConfiguration = Configuration::forSymmetricSigner(
+            new Sha256(),
+            InMemory::plainText('0eylDkJplsBm22Meby8EKIeBMckMKMyO')
+        );
 
-      $user = user_load_by_name($username);
+        // $keycloak_issuer = Settings::get('KEYCLOAK_ISSUER', "http://localhost:8003/auth/realms/master");
+        $keycloak_issuer = Settings::get('KEYCLOAK_ISSUER', "https://keycloak.lecontenaire.com/auth/realms/dev");
 
-      if (!$user) {
-        $values = [
-          'status' => 1,
-          'name' => $username,
-          'mail' => $mail,
-        ];
+        $this->jwtConfiguration->setValidationConstraints(
+            new IssuedBy($keycloak_issuer));
 
-        $user = User::create($values);
-        $user->save();
-      }
+        try {
+            // Attempt to parse the JWT
+            $token = $this->jwtConfiguration->parser()->parse($jwt);
+        } catch (\Lcobucci\JWT\Exception $exception) {
+            throw OAuthServerException::accessDenied($exception->getMessage(), null, $exception);
+        }
 
-      return $user;
-  }
-  
+        try {
+            // Attempt to validate the JWT
+            $constraints = $this->jwtConfiguration->validationConstraints();
+            $this->jwtConfiguration->validator()->assert($token, ...$constraints);
+        } catch (RequiredConstraintsViolated $exception) {
+            throw OAuthServerException::accessDenied('Access token could not be verified');
+        }
+
+        $claims = $token->claims();
+        $username = $claims->get("preferred_username");
+        $mail = $claims->get("email", $username . "@keycloak.com");
+
+        $user = user_load_by_name($username);
+
+        if (!$user) {
+            $values = [
+                'status' => 1,
+                'name' => $username,
+                'mail' => $mail,
+            ];
+
+            $user = User::create($values);
+            $user->save();
+        }
+
+        return $user;
+    }
+
+    /**
+     * Wrapper method for REQUEST_TIME constant.
+     *
+     * @return int
+     */
+    protected function getRequestTime() {
+        return defined('REQUEST_TIME') ? REQUEST_TIME : (int) $_SERVER['REQUEST_TIME'];
+    }
+
 }
