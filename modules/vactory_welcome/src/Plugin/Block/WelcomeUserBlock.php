d04ac271fda909519fe6338560e20ab6eabfcfaf
Gkla1502Void <m.dibe@void.fr>
2022-09-05 11:13:36 +0100

    Update: ajout du custom token [vactory_welcome:message] et le wysiwyg dans le block

--- modules/vactory_welcome/src/Plugin/Block/WelcomeUserBlock.php
+++ modules/vactory_welcome/src/Plugin/Block/WelcomeUserBlock.php
@@ -3,11 +3,8 @@
 namespace Drupal\vactory_welcome\plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Form\FormStateInterface;
-use Drupal\taxonomy\Entity\Term;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Drupal\Core\Datetime\DrupalDateTime;
-
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Class EspacePriveBlock.
@@ -22,76 +19,42 @@ use Drupal\Core\Datetime\DrupalDateTime;
 class WelcomeUserBlock extends BlockBase {
 
   /**
-   * Function block form.
+   * {@inheritdoc}
    */
-  // public function blockForm($form, FormStateInterface $form_state) {
-  //   parent::blockForm($form, $form_state);
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+    $config = $this->getConfiguration();
+    $form['welcome_description'] = [
+      '#type' => 'text_format',
+      '#title' => 'Enter a welcome description',
+      '#description' => $this->t('Enter a message, a quote or anything you want to tell to the user about'),
+      '#default_value' => isset($config['welcome_description']) ? $config['welcome_description']["value"] : '',
+    ];
+    $form['welcome_description']['tree_token'] = get_token_tree();
+    return $form;
+  }
 
-  //   $form['welcome_description'] = [
-  //     '#type' => 'text_format',
-  //     '#title' => 'Enter a welcome description',
-  //     '#description' => $this->t('Enter a message, a quote or anything you want to tell to the user about'),
-  //     // '#default_value' => $config->get('welcome_description'),
-  //   ];
-  //   $form['welcome_description']['tree_token'] = get_token_tree();
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    parent::blockSubmit($form, $form_state);
+    $values = $form_state->getValues();
+    $this->configuration['welcome_description'] = $values['welcome_description'];
+  }
 
-
-  //   return $form;
-
-  // }
-
-  // /**
-  //  * Function block submit.
-  //  */
-  // public function blockSubmit($form, FormStateInterface $form_state) {
-  //   parent::blockSubmit($form, $form_state);
-  //   $this->configuration['rapport_digital_year'] = $form_state->getValue('rapport_digital_year');
-  // }
-
-  // /**
-  //  * Welcome message block build().
-  //  */
+  /**
+   * Welcome message block build().
+   */
 
   public function build() {
-
-    $welcome_terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')
-      ->loadTree('welcome');
-    $current_lang_code = \Drupal::languageManager()
-      ->getCurrentLanguage()
-      ->getId();
-    $term_info = [];
-    if (!empty($welcome_terms) && isset($welcome_terms) ) {
-      foreach ($welcome_terms as $term) {
-        $term_id = Term::load($term->tid);
-        $translated_term = \Drupal::service('entity.repository')
-          ->getTranslationFromContext($term_id, $current_lang_code);
-          $start_time = new DrupalDateTime($translated_term->get('field_time_range_v')->getValue()[0]["value"], 'UTC');
-          $end_time = new DrupalDateTime($translated_term->get('field_time_range_v')->getValue()[0]["end_value"], 'UTC');
-        $term_info[] = [
-          'name' => $translated_term->getName(),
-          'tid' => $translated_term->id(),
-          'description' => $translated_term->get('description')->value,
-          'start_time' => strtotime($start_time->format('H:i')),
-          'end_time'=> strtotime($end_time->format('H:i')),
-        ];
-      }
-    }
-    $welcome_value = "";
-    $welcome_description= "";
-    $current_user = \Drupal::currentUser()->getAccountName();
-    $current_time = strtotime(date('H:i'));
-    foreach($term_info as $term) {
-      if ($current_time >= $term["start_time"] && $current_time < $term["end_time"]) {
-        $welcome_value = $term["name"];
-        $welcome_description = $term["description"];
-      }
-    }
-    return [
+   $config = $this->getConfiguration();
+   $welcome_value = (isset($config['welcome_description']) && !empty($config['welcome_description'])) ? $config['welcome_description'] : "";
+   $welcome_value = \Drupal::token()->replace($welcome_value["value"]);
+   return [
       '#theme' => 'welcome_user',
       "#content" => [
         '#value' => $welcome_value,
-        '#user' => $current_user,
-        '#description' => $welcome_description,
       ],
     ];
     throw new NotFoundHttpException();
