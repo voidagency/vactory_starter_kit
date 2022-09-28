8f48095e666191bbf01d8fad66a8be17842df253
Khalid bouhouch <k.bouhouch@void.fr>
2022-09-28 21:37:53 +0100

    feat(forums): decoupled

--- modules/vactory_forums/src/Controller/ForumSearchController.php
+++ modules/vactory_forums/src/Controller/ForumSearchController.php
@@ -2,18 +2,7 @@
 
 namespace Drupal\vactory_forums\Controller;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Language\LanguageManagerInterface;
-use Drupal\Core\Url;
-use Drupal\metatag\MetatagManagerInterface;
-//use Drupal\search_api\Plugin\views\query\SearchApiQuery;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Drupal\vactory_jsonapi\Controller\SearchController;
-//use Drupal\search_api\Entity\Index;
+use Drupal\vactory_decoupled_search\Controller\SearchController;
 
 /**
  * Class SearchController
