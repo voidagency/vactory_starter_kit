cbcf1c6827e686782012cd060f9cadd4a439c551
Khalid bouhouch <k.bouhouch@void.fr>
2022-10-26 15:56:07 +0100

    Fix: #39646- Get data on details sans avoir passer par le block cross-content

--- modules/vactory_decoupled/src/Plugin/Field/InternalNodeEntityBlocksFieldItemList.php
+++ modules/vactory_decoupled/src/Plugin/Field/InternalNodeEntityBlocksFieldItemList.php
@@ -30,21 +30,25 @@ class InternalNodeEntityBlocksFieldItemList extends FieldItemList
       return;
     }
 
+    $plugin_filter = [
+      'operator' => 'NOT IN',
+      'plugins' => [],
+    ];
+
     // Exclude Banner blocks.
-    $banner_plugin_filter = [];
     $banner_blocks = \Drupal::entityTypeManager()->getStorage('block_content')
       ->loadByProperties(['type' => 'vactory_decoupled_banner']);
     if (!empty($banner_blocks)) {
       $banner_blocks_plugins = array_map(function ($banner_block) {
         return 'block_content:' . $banner_block->uuid();
       }, $banner_blocks);
-      $banner_plugin_filter = [
-        'operator' => 'NOT IN',
-        'plugins' => array_values($banner_blocks_plugins),
-      ];
+      $plugin_filter['plugins'] = array_values($banner_blocks_plugins);
     }
 
-    $value = $block_manager->getBlocksByNode($entity->id(), $banner_plugin_filter);
+    // Exclude Cross Content Blocks.
+    array_push($plugin_filter['plugins'], 'vactory_cross_content');
+
+    $value = $block_manager->getBlocksByNode($entity->id(), $plugin_filter);
 
     // @see https://api.drupal.org/api/drupal/core%21modules%21system%21tests%21modules%21entity_test%21src%21Plugin%21Field%21ComputedTestCacheableStringItemList.php/class/ComputedTestCacheableStringItemList/9.3.x?title=&title_1=&object_type=&order=title&sort=desc
     $this->list[0] = $this->createItem(0, $value);
