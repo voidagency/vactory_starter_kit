d324a1eebb590e6b86c9e79eed431a495689ff95
Meryem Dibe <m.dibe@void.fr>
2023-05-10 12:42:28 +0100

    Update: fix Drupal Standard coding

--- modules/vactory_rate/src/Field/VoteEntityFieldItemList.php
+++ modules/vactory_rate/src/Field/VoteEntityFieldItemList.php
@@ -7,24 +7,26 @@ use Drupal\Core\Field\FieldItemList;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\TypedData\ComputedItemListTrait;
 use Drupal\Core\TypedData\TraversableTypedDataInterface;
-use Drupal\node\Entity\Node;
-use Drupal\serialization\Normalizer\CacheableNormalizerInterface;
 
 /**
  * Item list for a computed vote field.
- *
  */
-
-class VoteEntityFieldItemList extends FieldItemList
-{
+class VoteEntityFieldItemList extends FieldItemList {
   use ComputedItemListTrait;
 
+  /**
+   * {@inheritdoc}
+   */
   protected ?CacheableMetadata $cacheMetadata = NULL;
-
+  /**
+   * {@inheritdoc}
+   */
   protected $rate;
 
-  public static function createInstance($definition, $name = NULL, TraversableTypedDataInterface $parent = NULL)
-  {
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance($definition, $name = NULL, TraversableTypedDataInterface $parent = NULL) {
     $instance = parent::createInstance($definition, $name, $parent);
     $container = \Drupal::getContainer();
     $instance->rate = $container->get('vactory_rate.rate');
@@ -32,22 +34,20 @@ class VoteEntityFieldItemList extends FieldItemList
     return $instance;
   }
 
-
   /**
    * {@inheritdoc}
    */
-  protected function computeValue()
-  {
+  protected function computeValue() {
     /** @var \Drupal\node\NodeInterface $node */
     $entity = $this->getEntity();
     $bundle = $entity->bundle();
     $entity_type = $entity->getEntityTypeId();
     $entity_id = $entity->id();
     if ($bundle != 'vactory_academy' || $bundle != 'vactory_forum') {
-        return;
+      return;
     }
     $voting_results = $this->rate->results($entity_type, $entity_id);
-    $results = json_decode($voting_results->getContent(),true);
+    $results = json_decode($voting_results->getContent(), TRUE);
     $this->cacheMetadata->addCacheContexts(['user']);
     $this->cacheMetadata->addCacheTags(['vote_list']);
 
@@ -55,16 +55,15 @@ class VoteEntityFieldItemList extends FieldItemList
   }
 
   /**
-   * {@inheritdoc}  
+   * {@inheritdoc}
    */
-  public function access($operation = 'view', AccountInterface $account = NULL, $return_as_object = FALSE)
-  {
+  public function access($operation = 'view', AccountInterface $account = NULL, $return_as_object = FALSE) {
     $access = parent::access($operation, $account, TRUE);
 
     if ($return_as_object) {
-      /** @see \Drupal\jsonapi\JsonApiResource\ResourceIdentifier */
-      /** @see \Drupal\jsonapi\Normalizer\ResourceIdentifierNormalizer */
-      /** @see \Drupal\jsonapi\Normalizer\ResourceObjectNormalizer::serializeField() */
+      /* @see \Drupal\jsonapi\JsonApiResource\ResourceIdentifier */
+      /* @see \Drupal\jsonapi\Normalizer\ResourceIdentifierNormalizer */
+      /* @see \Drupal\jsonapi\Normalizer\ResourceObjectNormalizer::serializeField() */
       $this->ensureComputedValue();
       \assert($this->cacheMetadata instanceof CacheableMetadata);
       $access->addCacheableDependency($this->cacheMetadata);
