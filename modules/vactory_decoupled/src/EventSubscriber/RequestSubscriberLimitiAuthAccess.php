33f9feadecbb5c43671669e50d6668c5b664f13a
Brahim Khouy <b.khouy@void.fr>
2023-11-17 10:55:54 +0100

    Fix: fix Drupal 10 deprecated code

--- modules/vactory_decoupled/src/EventSubscriber/RequestSubscriberLimitiAuthAccess.php
+++ modules/vactory_decoupled/src/EventSubscriber/RequestSubscriberLimitiAuthAccess.php
@@ -3,7 +3,7 @@
 namespace Drupal\vactory_decoupled\EventSubscriber;
 
 use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Symfony\Cmf\Component\Routing\RouteObjectInterface;
+use Drupal\Core\Routing\RouteObjectInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
