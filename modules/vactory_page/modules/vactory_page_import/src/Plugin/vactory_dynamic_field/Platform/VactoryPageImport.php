aec8cfa94ec03395e417caeabc4290a5cd956979
ismailboulaanait <i.boulaanait@void.fr>
2024-01-02 15:34:15 +0100

    feat: #48312 - add module vactory_page_import

---
+++ modules/vactory_page/modules/vactory_page_import/src/Plugin/vactory_dynamic_field/Platform/VactoryPageImport.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\vactory_page_import\Plugin\vactory_dynamic_field\Platform;
+
+use Drupal\vactory_dynamic_field\VactoryDynamicFieldPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * A DF provider plugin.
+ *
+ * @PlatformProvider(
+ *   id = "vactory_page_import",
+ *   title = @Translation("Vactory Page Import")
+ * )
+ */
+class VactoryPageImport extends VactoryDynamicFieldPluginBase {
+
+  /**
+   * Extension path resolver service.
+   *
+   * @var \Drupal\Core\Extension\ExtensionPathResolver
+   */
+  protected $extensionPathResolver;
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->extensionPathResolver = $container->get('extension.path.resolver');
+    $uri = 'private://imported-pages-df';
+    if (!file_exists($uri)) {
+      mkdir($uri);
+    }
+    $instance->setWidgetsPath($uri);
+    return $instance;
+  }
+
+}
