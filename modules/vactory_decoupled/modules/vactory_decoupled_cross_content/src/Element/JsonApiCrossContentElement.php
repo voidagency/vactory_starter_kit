dbdeb63da578818673c9688b4d126b3a4f3498c4
ismailboulaanait <i.boulaanait@void.fr>
2024-07-03 23:16:15 +0100

    feat: #52050 - add decoupled cross content module

---
+++ modules/vactory_decoupled/modules/vactory_decoupled_cross_content/src/Element/JsonApiCrossContentElement.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\vactory_decoupled_cross_content\Element;
+
+use Drupal\vactory_decoupled\Element\JsonApiCollectionElement;
+
+/**
+ * Provide a JSON API form element for cross content.
+ *
+ * @FormElement("json_api_cross_content")
+ */
+class JsonApiCrossContentElement extends JsonApiCollectionElement {
+}
