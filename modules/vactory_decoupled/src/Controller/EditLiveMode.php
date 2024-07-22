a54a8f38a92abdd9b550366e249d574ebeb47ae8
Boulaanait ismail <boulaanait.ismail@gmail.com>
2024-07-22 15:35:41 +0100

    feat: #52587 - live mode edit - add revisions

--- modules/vactory_decoupled/src/Controller/EditLiveMode.php
+++ modules/vactory_decoupled/src/Controller/EditLiveMode.php
@@ -79,7 +79,35 @@ class EditLiveMode extends ControllerBase {
     $component_data = json_encode($component_data);
     $component[0]['widget_data'] = $component_data;
     $paragraph->field_vactory_component = $component;
+    $paragraph->setNewRevision(TRUE);
     $paragraph->save();
+    $last_paragraph_revision = $paragraph->getRevisionId();
+    $node = $this->entityTypeManager()->getStorage('node')->load($body['nid']);
+    $node = $this->getNodeTranslation($node);
+
+    if (is_null($node)) {
+      return [
+        'status' => 400,
+        'message' => $this->t('No translation founded for this page'),
+      ];
+    }
+
+    $node_paragraphs = $node->get('field_vactory_paragraphs')->getValue();
+    foreach ($node_paragraphs as &$item) {
+      if ($item['target_id'] == $body['paragraphId']) {
+        $item['target_revision_id'] = $last_paragraph_revision;
+        break;
+      }
+    }
+    $node->set('field_vactory_paragraphs', $node_paragraphs);
+
+    $node->setNewRevision(TRUE);
+    $node->revision_log = 'Update from live mode : ' . json_encode($body);
+    $node->setRevisionCreationTime(time());
+    $node->setRevisionUserId(\Drupal::currentUser()->id());
+    $node->set('revision_translation_affected', TRUE);
+    $node->save();
+
   }
 
   /**
@@ -108,7 +136,62 @@ class EditLiveMode extends ControllerBase {
     $tab_data = json_encode($tab_data);
     $tab[$body['templateDelta']]['widget_data'] = $tab_data;
     $paragraph->field_tab_templates = $tab;
+    // Saving the tab paragraph.
+    $paragraph->setNewRevision(TRUE);
     $paragraph->save();
+    $last_paragraph_revision = $paragraph->getRevisionId();
+
+    // Loading the main paragraph.
+    $main_paragraph_query = $this->entityTypeManager()->getStorage('paragraph')->getQuery();
+    $main_paragraph_query->accessCheck(FALSE);
+    $main_paragraph_query->condition('id', $body['paragraphId'])
+      ->condition('parent_id', $body['nid'])
+      ->condition('parent_type', 'node')
+      ->condition('type', 'vactory_paragraph_multi_template');
+
+    $result = $this->fetchParagraph($main_paragraph_query);
+    if (!$result instanceof Paragraph) {
+      return $result;
+    }
+    $main_paragraph = $result;
+    $main_paragraph_tabs = $main_paragraph->get('field_vactory_paragraph_tab')->getValue();
+    foreach ($main_paragraph_tabs as &$item) {
+      if ($item['target_id'] == $body['paragraphTabId']) {
+        $item['target_revision_id'] = $last_paragraph_revision;
+        break;
+      }
+    }
+
+    $main_paragraph->set('field_vactory_paragraph_tab', $main_paragraph_tabs);
+    $main_paragraph->setNewRevision(TRUE);
+    $main_paragraph->save();
+    $last_main_paragraph_revision = $main_paragraph->getRevisionId();
+
+    $node = $this->entityTypeManager()->getStorage('node')->load($body['nid']);
+    $node = $this->getNodeTranslation($node);
+
+    if (is_null($node)) {
+      return [
+        'status' => 400,
+        'message' => $this->t('No translation founded for this page'),
+      ];
+    }
+
+    $node_paragraphs = $node->get('field_vactory_paragraphs')->getValue();
+    foreach ($node_paragraphs as &$item) {
+      if ($item['target_id'] == $body['paragraphId']) {
+        $item['target_revision_id'] = $last_main_paragraph_revision;
+        break;
+      }
+    }
+    $node->set('field_vactory_paragraphs', $node_paragraphs);
+
+    $node->setNewRevision(TRUE);
+    $node->revision_log = 'Update from live mode : ' . json_encode($body);
+    $node->setRevisionCreationTime(time());
+    $node->setRevisionUserId(\Drupal::currentUser()->id());
+    $node->set('revision_translation_affected', TRUE);
+    $node->save();
   }
 
   /**
@@ -158,6 +241,24 @@ class EditLiveMode extends ControllerBase {
         'code' => 400,
         'message' => $this->t('No translation founded'),
       ];
+    }
+  }
+
+  /**
+   * Retrieves the translation of a node for the current language.
+   */
+  private function getNodeTranslation($node) {
+    $language_manager = $this->languageManager();
+    $current_language = $language_manager->getCurrentLanguage()->getId();
+    $default_language = $language_manager->getDefaultLanguage()->getId();
+    if ($current_language == $default_language) {
+      return $node;
+    }
+    elseif ($node->hasTranslation($current_language)) {
+      return $node->getTranslation($current_language);
+    }
+    else {
+      return NULL;
     }
   }
 
