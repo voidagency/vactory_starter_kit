a5921034bef45da7c39f87a64940204d98cb00b5
SalahEddine <s.ghannouch@void.fr>
2024-11-26 16:21:56 +0100

    Fix: refactoring code

--- modules/vactory_remote_media_transcription/src/Plugin/Field/FieldFormatter/TruncatedTextFormatter.php
+++ modules/vactory_remote_media_transcription/src/Plugin/Field/FieldFormatter/TruncatedTextFormatter.php
@@ -2,8 +2,11 @@
 
 namespace Drupal\vactory_remote_media_transcription\Plugin\Field\FieldFormatter;
 
+use Drupal\Component\Utility\Unicode;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FormatterInterface;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Plugin implementation of the 'truncated_text' formatter.
@@ -18,28 +21,59 @@ use Drupal\Core\Field\FormatterBase;
  *   }
  * )
  */
-class TruncatedTextFormatter extends FormatterBase {
+class TruncatedTextFormatter extends FormatterBase implements FormatterInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'character_limit' => 50, // Default character limit.
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $form['character_limit'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Character limit'),
+      '#default_value' => $this->getSetting('character_limit'),
+      '#description' => $this->t('Set the maximum number of characters to display.'),
+      '#required' => TRUE,
+      '#min' => 1,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    return [
+      $this->t('Character limit: @limit', ['@limit' => $this->getSetting('character_limit')]),
+    ];
+  }
 
   /**
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = [];
+    $character_limit = $this->getSetting('character_limit');
 
     foreach ($items as $delta => $item) {
-      // Limit the text to 50 characters.
-      $text = mb_substr($item->value, 0, 50);
-      if (mb_strlen($item->value) > 50) {
-        // Add ellipsis if the text is truncated.
-        $text .= '...'; 
-      }
+      // Use Unicode::truncate to truncate text safely.
+      $text = Unicode::truncate($item->value, $character_limit, TRUE, TRUE);
 
+      // Sanitize the text for XSS protection.
       $elements[$delta] = [
-        '#markup' => $text,
+        '#markup' => htmlspecialchars($text, ENT_QUOTES, 'UTF-8'),
       ];
     }
 
     return $elements;
   }
-
 }
