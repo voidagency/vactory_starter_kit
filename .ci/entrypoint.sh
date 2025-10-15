5f7805b5670e2c9880316212dc4c86ef295839d9
Hamza Bahlouane <h.bahlaouane@void.fr>
2025-10-15 10:21:45 +0100

    fix: phpcs

--- .ci/entrypoint.sh
+++ .ci/entrypoint.sh
@@ -269,6 +269,7 @@ COMPOSER_START_TIME=$(date +%s)
 
 print_step "Configuring composer for local development" "⚙️" "\033[35m"
 php ${UPDATE_COMPOSER_SCRIPT}
+rm ${UPDATE_COMPOSER_SCRIPT}
 
 echo -e "\033[36m📄 Updated composer.json:\033[0m"
 cat composer.json
